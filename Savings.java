b26f90ca03d7cd20175986d1c3e366cf8fbe2632
24ftt1803 <24ftt1803@student.pb.edu.bn>
2025-08-10 20:43:31 +0800

    Create Savings.java

---
+++ Savings.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Savings {
+    public static void main(String[] args) {
+        final int DAYS_IN_MONTH = 30;
+        try (Scanner input = new Scanner(System.in)) {
+            
+            System.out.print("What is your target amount? $");
+            double targetAmount = input.nextDouble();
+
+            System.out.print("How many month(s) to reach your goal? ");
+            int months = input.nextInt();
+
+            double perDay = targetAmount / (months * DAYS_IN_MONTH);
+
+            System.out.printf("The amount you need to save per day is $%.2f", perDay);
+        }
+    }
+}
